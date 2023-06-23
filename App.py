ce4aead267af8e28a00ce1c28c8b8f383bb378ef
hoyinli1211 <hoyinli.r@gmail.com>
2023-06-24 02:05:08 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,18 +1,17 @@
-import urllib.request
-import json
+import streamlit as st
+import requests
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
-from bs4 import BeautifulSoup
-import requests
+import urllib.request
+import json
 
 
-def fetch_press_release_content(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'html.parser')
-    content = soup.get_text()
-    return content
+def fetch_press_releases():
+    url = 'https://api.hkma.gov.hk/public/press-releases?lang=en&offset=0'
+    with urllib.request.urlopen(url) as req:
+        data = json.loads(req.read())["result"]["records"]
+    return data
 
 
 def topic_modeling(data, n_topics=5):
@@ -23,25 +22,32 @@ def topic_modeling(data, n_topics=5):
     return topic_labels
 
 
-url = 'https://api.hkma.gov.hk/public/press-releases?lang=en&offset=0'
+def main():
+    st.title("HKMA Press Releases Topic Categorization")
 
-with urllib.request.urlopen(url) as req:
-    data = json.loads(req.read())
+    st.write("Fetching press releases data from HKMA OpenAPI...")
+    press_releases_data = fetch_press_releases()
 
-records = data['result']['records']
-df = pd.DataFrame(records)
+    data = []
+    titles = []
+    for item in press_releases_data:
+        titles.append(item["title"])
+        content_url = item["link"]
+        response = requests.get(content_url)
+        content = response.text if response.status_code == 200 else ''
+        data.append(content)
 
-# Fetch press release content
-content_list = []
-for link in df['link']:
-    content = fetch_press_release_content(link)
-    content_list.append(content)
+    if data:
+        st.write(f"Number of press releases: {len(data)}")
+        n_topics = st.slider("Select the number of topics:", 1, 10, 5)
+        topic_labels = topic_modeling(data, n_topics=n_topics)
 
-# Perform topic modeling
-n_topics = 5
-topic_labels = topic_modeling(content_list, n_topics=n_topics)
+        st.write("Topic labels assigned to press releases:")
+        for idx, topic_label in enumerate(topic_labels):
+            st.write(f"Press release {idx + 1}: Topic {topic_label + 1}")
+    else:
+        st.error("No data available.")
 
-# Add topic labels to the DataFrame
-df['topic'] = topic_labels
 
-print(df)
+if __name__ == "__main__":
+    main()
